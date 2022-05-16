591bb6c98981d80ed7687e67de8a290cc1cb17ec
lenal <lena.leibetseder@live.de>
2022-05-16 15:01:34 +0200

    Initial Commit

---
+++ Gruntfile.js
@@ -0,0 +1,38 @@
+module.exports = function(grunt) {
+
+    grunt.initConfig({
+        watch: {
+            files: ['**/*.scss'],
+            tasks: ['sass']
+        },
+        sass: {
+            dist: {
+                files: {
+                    'public/styles/main.css': ['scss/*.scss']
+                }
+            }
+        },
+        uglify: {
+            my_target: {
+                files: {
+                    'public/js/script.min.js': ['js/*.js']
+                }
+            }
+        },
+        cssmin: {
+            target: {
+                files: {
+                    'public/styles/main.min.css': 'public/styles/main.css'
+                }
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+
+    grunt.registerTask('default', ['sass', 'cssmin', 'uglify']);
+
+};
